eaf168063f99047562afcaa9f6186fa61dd198c1
celic <cceli@codequarry.net>
2016-01-06 22:36:40 -0500

    Some puller changer to condensing data

--- lib/pullers/daily_puller.rb
+++ lib/pullers/daily_puller.rb
@@ -19,11 +19,16 @@ goalie_hashes = []
 
 skaters.css('tr').each do |skater_row|
 
+  time = skater_row.css('td')[22].content.split(':')
+  time_in_seconds = (time[0].to_i * 60) + time[1].to_i
+
+  game_location = (skater_row.css('td')[4].content == "" ? "H" : "A")
+
   data_hash = {
     name: skater_row.css('td')[1].content,
     position: skater_row.css('td')[2].content,
     team: skater_row.css('td')[3].content,
-    game_location: skater_row.css('td')[4].content,
+    game_location: game_location,
     opponent: skater_row.css('td')[5].content,
     game_result: skater_row.css('td')[6].content,
     goals: skater_row.css('td')[7].content,
@@ -41,7 +46,7 @@ skaters.css('tr').each do |skater_row|
     shots: skater_row.css('td')[19].content,
     shot_pct: skater_row.css('td')[20].content,
     shifts: skater_row.css('td')[21].content,
-    toi: skater_row.css('td')[22].content
+    toi: time_in_seconds
   }
 
   skater_hashes << data_hash
@@ -50,11 +55,16 @@ end
 
 goalies.css('tr').each do |goalie_row|
 
+  time = goalie_row.css('td')[14].content.split(':')
+  time_in_seconds = (time[0].to_i * 60) + time[1].to_i
+
+  game_location = (goalie_row.css('td')[4].content == "" ? "H" : "A")
+
   data_hash = {
     name: goalie_row.css('td')[1].content,
     position: goalie_row.css('td')[2].content,
     team: goalie_row.css('td')[3].content,
-    game_location: goalie_row.css('td')[4].content,
+    game_location: game_location,
     opponent: goalie_row.css('td')[5].content,
     game_result: goalie_row.css('td')[6].content,
     goalie_decision: goalie_row.css('td')[7].content,
@@ -64,8 +74,10 @@ goalies.css('tr').each do |goalie_row|
     save_pct: goalie_row.css('td')[11].content,
     shutout: goalie_row.css('td')[12].content,
     pim: goalie_row.css('td')[13].content,
-    toi: goalie_row.css('td')[14].content,
+    toi: time_in_seconds
   }
 
   goalie_hashes << data_hash
 end
+
+puts goalie_hashes
