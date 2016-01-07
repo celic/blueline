548f0cdc43c506770a17726e3065b088eb7dc1f9
celic <cceli@codequarry.net>
2016-01-07 00:11:47 -0500

    Field matching with database

--- lib/pullers/daily_stats.rb
+++ lib/pullers/daily_stats.rb
@@ -32,26 +32,26 @@ module Pullers
                     team: skater_row.css('td')[3].content,
                     home: (skater_row.css('td')[4].content != '@'),
                     opponent: skater_row.css('td')[5].content,
-                    game_result: skater_row.css('td')[6].content,
+                    decision: skater_row.css('td')[6].content,
                     goals: skater_row.css('td')[7].content,
                     assists: skater_row.css('td')[8].content,
                     points: skater_row.css('td')[9].content,
-                    plus_minus: skater_row.css('td')[10].content,
+                    plusminus: skater_row.css('td')[10].content,
                     pim: skater_row.css('td')[11].content,
-                    goals_ev: skater_row.css('td')[12].content,
-                    goals_pp: skater_row.css('td')[13].content,
-                    goals_sh: skater_row.css('td')[14].content,
-                    goals_gw: skater_row.css('td')[15].content,
-                    assists_ev: skater_row.css('td')[16].content,
-                    assists_pp: skater_row.css('td')[17].content,
-                    assists_sh: skater_row.css('td')[18].content,
+                    evg: skater_row.css('td')[12].content,
+                    ppg: skater_row.css('td')[13].content,
+                    shg: skater_row.css('td')[14].content,
+                    gwg: skater_row.css('td')[15].content,
+                    eva: skater_row.css('td')[16].content,
+                    ppa: skater_row.css('td')[17].content,
+                    sha: skater_row.css('td')[18].content,
                     shots: skater_row.css('td')[19].content,
-                    shot_pct: skater_row.css('td')[20].content,
+                    shot_percentage: skater_row.css('td')[20].content,
                     shifts: skater_row.css('td')[21].content,
                     toi: skater_row.css('td')[22].attribute('csk').value
                 }
 
-                #store_player row
+                store_player row
             end
 
             puts skater_hashes[0]
@@ -66,20 +66,20 @@ module Pullers
                     name: goalie_row.css('td')[1].content,
                     position: goalie_row.css('td')[2].content,
                     team: goalie_row.css('td')[3].content,
-                    game_location: (goalie_row.css('td')[4].content != '@'),
+                    home: (goalie_row.css('td')[4].content != '@'),
                     opponent: goalie_row.css('td')[5].content,
-                    game_result: goalie_row.css('td')[6].content,
-                    goalie_decision: goalie_row.css('td')[7].content,
-                    goals_allowed: goalie_row.css('td')[8].content,
-                    shots_faced: goalie_row.css('td')[9].content,
+                    decision: goalie_row.css('td')[6].content,
+                    record: goalie_row.css('td')[7].content,
+                    goals_against: goalie_row.css('td')[8].content,
+                    shots_against: goalie_row.css('td')[9].content,
                     saves: goalie_row.css('td')[10].content,
-                    save_pct: goalie_row.css('td')[11].content,
+                    save_percentage: goalie_row.css('td')[11].content,
                     shutout: goalie_row.css('td')[12].content,
                     pim: goalie_row.css('td')[13].content,
                     toi: goalie_row.css('td')[14].attribute('csk').value
                 }
 
-                #store_goalie row
+                store_goalie row
             end
 
             Rails.logger.info "##### Pullers::DailyStats.run => Parsing complete"
