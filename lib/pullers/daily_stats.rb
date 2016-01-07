e7eb4ada84ecff0c75b1e03a9472b39084c8f96c
Andrew Rempe <andrewrempe@gmail.com>
2016-01-06 23:27:13 -0500

    Abstracts daily stats puller more

--- lib/pullers/daily_stats.rb
+++ lib/pullers/daily_stats.rb
@@ -6,16 +6,12 @@ require 'time'
 module Pullers
     class DailyStats
 
-        def self.string_to_seconds(time_str)
-          (time_str.split(':')[0].to_i * 60) + time_str.split(':')[1].to_i
-        end
+        def self.run(date = Date.today - 1.day)
 
-        def self.run
+            Rails.logger.info "##### Pullers::DailyStats.run => Running for #{date}"
 
-            yesterday = Date.today - 1.day
-            url = "http://www.hockey-reference.com/friv/dailyleaders.cgi?month=#{yesterday.month}&day=#{yesterday.day}&year=#{yesterday.year}"
+            url = "http://www.hockey-reference.com/friv/dailyleaders.cgi?month=#{date.month}&day=#{date.day}&year=#{date.year}"
 
-            Rails.logger.info "##### Pullers::DailyStats.run => Running for #{yesterday}"
             Rails.logger.info "##### Pullers::DailyStats.run => Opening: #{url}"
 
             page = Nokogiri::HTML(open(url))
@@ -28,61 +24,65 @@ module Pullers
 
             skaters.css('tr').each do |skater_row|
 
-            data_hash = {
-                name: skater_row.css('td')[1].content,
-                position: skater_row.css('td')[2].content,
-                team: skater_row.css('td')[3].content,
-                game_location: (skater_row.css('td')[4].content == "" ? "H" : "A"),
-                opponent: skater_row.css('td')[5].content,
-                game_result: skater_row.css('td')[6].content,
-                goals: skater_row.css('td')[7].content,
-                assists: skater_row.css('td')[8].content,
-                points: skater_row.css('td')[9].content,
-                plus_minus: skater_row.css('td')[10].content,
-                pim: skater_row.css('td')[11].content,
-                goals_ev: skater_row.css('td')[12].content,
-                goals_pp: skater_row.css('td')[13].content,
-                goals_sh: skater_row.css('td')[14].content,
-                goals_gw: skater_row.css('td')[15].content,
-                assists_ev: skater_row.css('td')[16].content,
-                assists_pp: skater_row.css('td')[17].content,
-                assists_sh: skater_row.css('td')[18].content,
-                shots: skater_row.css('td')[19].content,
-                shot_pct: skater_row.css('td')[20].content,
-                shifts: skater_row.css('td')[21].content,
-                toi: string_to_seconds(skater_row.css('td')[22].content)
-            }
+                row = {
+                    name: skater_row.css('td')[1].content,
+                    position: skater_row.css('td')[2].content,
+                    team: skater_row.css('td')[3].content,
+                    home: (skater_row.css('td')[4].content != '@'),
+                    opponent: skater_row.css('td')[5].content,
+                    game_result: skater_row.css('td')[6].content,
+                    goals: skater_row.css('td')[7].content,
+                    assists: skater_row.css('td')[8].content,
+                    points: skater_row.css('td')[9].content,
+                    plus_minus: skater_row.css('td')[10].content,
+                    pim: skater_row.css('td')[11].content,
+                    goals_ev: skater_row.css('td')[12].content,
+                    goals_pp: skater_row.css('td')[13].content,
+                    goals_sh: skater_row.css('td')[14].content,
+                    goals_gw: skater_row.css('td')[15].content,
+                    assists_ev: skater_row.css('td')[16].content,
+                    assists_pp: skater_row.css('td')[17].content,
+                    assists_sh: skater_row.css('td')[18].content,
+                    shots: skater_row.css('td')[19].content,
+                    shot_pct: skater_row.css('td')[20].content,
+                    shifts: skater_row.css('td')[21].content,
+                    toi: skater_row.css('td')[22].attribute('csk').value
+                }
 
-              skater_hashes << data_hash
-
+                evaluate_player row
             end
+
+            puts skater_hashes[0]
 
             goalies.css('tr').each do |goalie_row|
 
-              data_hash = {
-                name: goalie_row.css('td')[1].content,
-                position: goalie_row.css('td')[2].content,
-                team: goalie_row.css('td')[3].content,
-                game_location: (goalie_row.css('td')[4].content == "" ? "H" : "A"),
-                opponent: goalie_row.css('td')[5].content,
-                game_result: goalie_row.css('td')[6].content,
-                goalie_decision: goalie_row.css('td')[7].content,
-                goals_allowed: goalie_row.css('td')[8].content,
-                shots_faced: goalie_row.css('td')[9].content,
-                saves: goalie_row.css('td')[10].content,
-                save_pct: goalie_row.css('td')[11].content,
-                shutout: goalie_row.css('td')[12].content,
-                pim: goalie_row.css('td')[13].content,
-                toi: string_to_seconds(goalie_row.css('td')[14].content)
-              }
+                 row = {
+                    name: goalie_row.css('td')[1].content,
+                    position: goalie_row.css('td')[2].content,
+                    team: goalie_row.css('td')[3].content,
+                    game_location: (goalie_row.css('td')[4].content != '@'),
+                    opponent: goalie_row.css('td')[5].content,
+                    game_result: goalie_row.css('td')[6].content,
+                    goalie_decision: goalie_row.css('td')[7].content,
+                    goals_allowed: goalie_row.css('td')[8].content,
+                    shots_faced: goalie_row.css('td')[9].content,
+                    saves: goalie_row.css('td')[10].content,
+                    save_pct: goalie_row.css('td')[11].content,
+                    shutout: goalie_row.css('td')[12].content,
+                    pim: goalie_row.css('td')[13].content,
+                    toi: goalie_row.css('td')[14].attribute('csk').value
+                }
 
-              goalie_hashes << data_hash
+                evaluate_goalie row
             end
 
-            goalie_hashes
-
             Rails.logger.info "##### Pullers::DailyStats.run => Parsing complete"
+        end
 
+        def evaluate_player(data)
+        end
+
+        def evaluate_goalie(data)
         end
     end
 end
